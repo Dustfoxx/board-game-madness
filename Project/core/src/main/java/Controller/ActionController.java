745b2afb26b4480282666ef08a753ffa19a62038
Jocke <joakim-johansson99@hotmail.com>
2024-11-19 13:25:32 +0100

    interface added

---
+++ Project/core/src/main/java/Controller/ActionController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+public class ActionController {
+
+
+    /**
+     * Lets user ask about a feature its standing on
+     * @param tile The tile standing on
+     * @return The feature asked upon
+     */
+    public int ask(){
+        // Check features players standing on
+        // Player chooses feature through view
+        // Return requested feature
+        return 1;
+    }
+    
+    /**
+     * Reveals the specific time of footstep token
+     * @param cell the cell requested to reveal ?? Footstep maybe??
+     * @return True if succesful, else false
+     */
+    public boolean reveal(){
+        // Get timestamp from game state model 
+        // Set token on cell
+        return true;
+    }
+}
---
+++ Project/core/src/main/java/Controller/RecruiterActionController.java
@@ -0,0 +1,51 @@
+package Controller;
+
+public class RecruiterActionController {
+
+    /**
+     * Moves the recruiter
+     * Sets token if passed
+     * @param feature The feature asked about
+     * @return True if moved, else false
+     */
+    public boolean move(){
+        return true;
+    }
+
+    /**
+     * Asks the recruiter about crossed features
+     * Sets token if passed
+     * @param void
+     * @return True if moved, else false
+     */
+    public boolean mindSlip(){
+        // Check Valid Mindslip moves
+
+        // Send valid moves to view (recieve player move)
+
+        // Update board state
+        
+        return true;
+    }
+
+    /**
+     * Asks the recruiter about crossed features
+     * Sets token if passed
+     * @param feature The feature asked about
+     * @return True if crossed, else false
+     */
+    public int answer(){
+
+        // Iterate through recruiter path, return feature cells without footstep
+
+        // If no footsteps return false
+
+        // else
+        // Let recruiter pick cell through view
+
+        // set token, update state
+        // Return true
+        return 1;
+    }
+    
+}
