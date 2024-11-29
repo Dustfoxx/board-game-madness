01fa47b966aba7129a840716e42bf1f3f6ff0fc7
Fox <fox.hamren@gmail.com>
2024-11-29 18:45:05 +0100

    added movement options
    Recruiter movement check
    Agent movement check
    Movement tests

--- Project/core/src/main/java/Controller/CheckMove.java
+++ Project/core/src/main/java/Controller/CheckMove.java
@@ -1,116 +1,220 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import Model.Board;
 import Model.AbstractCell;
+import Model.TempleCell;
 import Model.Player;
-
-//IMPORTANT:: AS OF THIS PULL REQUEST THIS FILE IS UNTESTED AS TEMPLE TILES ARE NOT ADDED
-//THIS MEANS IT WILL BE TESTED FROM A SEPARATE BRANCH BUT WILL BE DELIVERED VIA THIS ONE
+import Model.Recruiter;
+import Model.RougeAgent;
 
 public class CheckMove {
-    /*
-     * private List<int[]> checkSurroundingCells(int[] coord, Board board) {
-     * int[] boardDims = board.getDims();
+
+    /**
+     * Takes player position and returns a list of all valid single step coordinates
+     * All orthogonal and including diagonal if some are temples
      * 
-     * int[] validRows = getCoordRange(coord[0], boardDims[0]);
-     * int[] validCols = getCoordRange(coord[1], boardDims[1]);
-     * 
-     * List<int[]> possibleCoords = new ArrayList<int[]>();
-     * 
-     * for (int row = validRows[0]; row <= validRows[1]; row++) {
-     * for (int col = validCols[0]; col <= validCols[1]; col++) {
-     * if (board.getCell(coord[0], coord[1]) instanceof TempleCell) {
-     * possibleCoords.add(new int[] { row, col });
-     * } else {
-     * if (row == coord[0] || col == coord[1]) {
-     * possibleCoords.add(new int[] { row, col });
-     * } else if (board.getCell(row, col) instanceof TempleCell) {// Should check
-     * for temple here
-     * possibleCoords.add(new int[] { row, col });
-     * }
-     * }
-     * }
-     * }
-     * return possibleCoords;
-     * }
-     * 
-     * private int[] getCoordRange(int location, int maxVal) {
-     * int[] range = { 0, 0 };
-     * 
-     * if (location != 0) {
-     * range[0] = location - 1;
-     * }
-     * if (location < maxVal) {
-     * range[1] = location + 1;
-     * } else {
-     * range[1] = maxVal;
-     * }
-     * 
-     * return range;
-     * }
-     * 
-     * public void printBoard(Cell[][] board) {
-     * for (Cell[] row : board) {
-     * for (Cell cell : row) {
-     * System.out.print(cell instanceof Temple ? "0 " : "1 ");
-     * }
-     * System.out.println("\n");
-     * }
-     * System.out.println("\n");
-     * }
-     * 
-     * public int[][] getValidMoves(Player player, Board board) {// In testing i
-     * will assume matrix of ones and zeros
-     * int[] dims = board.getDims();
-     * 
-     * int[] coords = null;
-     * List<Player> cellPlayers = null;
-     * 
-     * // I hate this function so please if you have a better idea fix it
-     * // Move through rows
-     * for (int row = 0; row < dims[0]; row++) {
-     * // Move through columns
-     * for (int col = 0; col < dims[1]; col++) {
-     * // Get players from cell
-     * cellPlayers = board.getCell(row, col).getPlayers();
-     * // Iterate over players
-     * for (Player foundPlayer : cellPlayers) {
-     * // If player is equal save coords
-     * coords = foundPlayer.getId() == player.getId() ? new int[] { row, col } :
-     * null;
-     * // If player has been found exit loops
-     * if (coords != null) {
-     * row = dims[0];
-     * col = dims[1];
-     * break;
-     * }
-     * }
-     * }
-     * }
-     * 
-     * if (coords == null) {
-     * throw new IllegalStateException("Player ID not found");
-     * }
-     * 
-     * List<int[]> firstMove = checkSurroundingCells(coords, board);
-     * List<int[]> secondMove = new ArrayList<int[]>();
-     * 
-     * for (int i = 0; i < firstMove.size(); i++) {
-     * secondMove.addAll(checkSurroundingCells(firstMove.get(i), board));
-     * }
-     * 
-     * firstMove.addAll(secondMove);
-     * 
-     * int[][] possibleMoves = new int[dims[0]][dims[1]];
-     * 
-     * for (int i = 0; i < firstMove.size(); i++) {
-     * possibleMoves[firstMove.get(i)[0]][firstMove.get(i)[1]] = 1;
-     * }
-     * 
-     * return possibleMoves;
-     * }
+     * @param coord Player coordinates
+     * @param board Board player inhabits
+     * @return List<int[]> List of possible choices as coordinates
      */
+    private List<int[]> checkSurroundingCells(int[] coord, Board board) {
+        int[] boardDims = board.getDims();
+        int[] validRows = getCoordRange(coord[0], boardDims[0]);
+        int[] validCols = getCoordRange(coord[1], boardDims[1]);
+        List<int[]> possibleCoords = new ArrayList<int[]>();
+
+        for (int row = validRows[0]; row <= validRows[1]; row++) {
+            for (int col = validCols[0]; col <= validCols[1]; col++) {
+                if (!Arrays.equals(coord, new int[] { row, col })) {
+                    if (board.getCell(coord[0], coord[1]) instanceof TempleCell) {
+                        possibleCoords.add(new int[] { row, col });
+                    } else {
+                        if (row == coord[0] || col == coord[1]) {
+                            possibleCoords.add(new int[] { row, col });
+                        } else if (board.getCell(row, col) instanceof TempleCell) {// Should check for temple here
+                            possibleCoords.add(new int[] { row, col });
+                        }
+                    }
+                }
+            }
+        }
+        return possibleCoords;
+    }
+
+    /**
+     * Gets the range of movement coordinates to iterate over, I.e. position 3 gives
+     * array of 2-4.
+     * 
+     * @param location Position in single dimension
+     * @param maxVal   What the maximum possible coordinate is
+     * @return Returns an array of three integers
+     */
+    private int[] getCoordRange(int location, int maxVal) {
+        int[] range = { 0, 0 };
+
+        if (location != 0) {
+            range[0] = location - 1;
+        }
+        if (location < maxVal) {
+            range[1] = location + 1;
+        } else {
+            range[1] = maxVal;
+        }
+
+        return range;
+    }
+
+    /**
+     * Prints the board based on cells. Mainly used for testing.
+     * 
+     * @param board The board that should be printed
+     */
+    public void printBoard(AbstractCell[][] board) {
+        for (AbstractCell[] row : board) {
+            for (AbstractCell cell : row) {
+                System.out.print(cell instanceof TempleCell ? "0 " : "1 ");
+            }
+            System.out.println("\n");
+        }
+        System.out.println("\n");
+    }
+
+    /**
+     * Prints the created movement mask. Only used for testing.
+     * 
+     * @param mask The mask that should be printed
+     */
+    public void printMask(int[][] mask) {
+        for (int[] row : mask) {
+            for (int cell : row) {
+                System.out.print(cell);
+            }
+            System.out.println("\n");
+        }
+        System.out.println("\n");
+    }
+
+    /**
+     * Creates a mask from a list of coordinates and the size of the board
+     * 
+     * @param rows       amount of rows
+     * @param cols       amount of columns
+     * @param foundMoves allowed movement coordinates
+     * @return a matrix of zeroes and ones. ones are possible movement
+     */
+    private int[][] createMask(int rows, int cols, List<int[]> foundMoves) {
+        int[][] possibleMoves = new int[rows][cols];
+
+        for (int i = 0; i < foundMoves.size(); i++) {
+            possibleMoves[foundMoves.get(i)[0]][foundMoves.get(i)[1]] = 1;
+        }
+
+        return possibleMoves;
+    }
+
+    /**
+     * A single movement for any player. Extracted since its the same for recruiter
+     * and agent
+     * 
+     * @param player Which player is currently choosing movement
+     * @param board  the board the player inhabits
+     * @return List of possible movement coordinates
+     */
+    private List<int[]> initialMove(Player player, Board board) {
+        int[] coords = board.getPlayerCoord(player);
+
+        if (coords == null) {
+            throw new IllegalStateException("Player ID not found");
+        }
+
+        return checkSurroundingCells(coords, board);
+    }
+
+    /**
+     * Figures out possible mindslip moves
+     * 
+     * @param player Recuiter in the current game
+     * @param board  Board the player inhabits
+     * @param type   Type of mindslip equipped. 0 for orhogonal and 1 for diagonal
+     * @return Possible coordinates to visit after mindslip
+     */
+    private List<int[]> mindSlip(Recruiter player, Board board, int type) {
+        int mindSlipType = type;// player.getMindSlip() NEEDS TO BE AVAILABLE
+        int[] coords = board.getPlayerCoord(player);
+        int[] dims = board.getDims();
+
+        int[] movement = new int[] { -2, 2 };
+
+        List<int[]> possibleSlips = new ArrayList<int[]>();
+        int[] newCoord;
+
+        for (int i = 0; i < movement.length; i++) {
+            for (int j = 0; j < movement.length; j++) {
+                if (j + i % 2 == 0) {
+                    newCoord = new int[] { coords[0] + movement[i], coords[1] + movement[j] * mindSlipType };
+                } else {
+                    newCoord = new int[] { coords[0] + movement[i] * mindSlipType, coords[1] + movement[j] };
+                }
+                if (newCoord[0] >= 0 && newCoord[0] < dims[0] && newCoord[1] >= 0 && newCoord[1] < dims[1]) {
+                    possibleSlips.add(newCoord);
+                }
+            }
+        }
+        return possibleSlips;
+
+    }
+
+    /**
+     * Gets valid moves for an agent
+     * 
+     * @param player Agent that is being considered
+     * @param board  Board that the player inhabits
+     * @return Returns a mask that contains zeroes and ones saying where a player
+     *         can move
+     */
+    public int[][] getValidMoves(RougeAgent player, Board board) {
+        int[] dims = board.getDims();
+
+        List<int[]> firstMove = initialMove(player, board);
+        List<int[]> secondMove = new ArrayList<int[]>();
+        for (int i = 0; i < firstMove.size(); i++) {
+            secondMove.addAll(checkSurroundingCells(firstMove.get(i), board));
+        }
+
+        firstMove.addAll(secondMove);
+
+        return createMask(dims[0], dims[1], firstMove);
+    }
+
+    /**
+     * Gets valid moves for the recruiter
+     * 
+     * @param player recruiter
+     * @param board  Board that the player inhabits
+     * @param type   type of mindslip, 0 for orthogonal, 1 for diagonal
+     * @return Returns a mask that contains zeroes and ones saying where a player
+     *         can move
+     */
+    public int[][] getValidMoves(Recruiter player, Board board, int type) {
+        int[] dims = board.getDims();
+
+        List<int[]> firstMove = initialMove(player, board);
+        List<int[]> walkedList = player.getWalkedPath();
+
+        firstMove.addAll(mindSlip(player, board, type));
+
+        for (int i = 0; i < walkedList.size(); i++) {
+            for (int j = 0; j < firstMove.size(); j++) {
+                if (Arrays.equals(walkedList.get(i), firstMove.get(j))) {
+                    firstMove.remove(j);
+                }
+            }
+        }
+
+        return createMask(dims[0], dims[1], firstMove);
+    }
 }
---
+++ Project/core/src/test/java/Controller/CheckMoveTests.java
@@ -0,0 +1,100 @@
+package Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Model.AbstractCell;
+import Model.Board;
+import Model.NormalCell;
+import Model.Feature;
+import Model.Recruiter;
+import Model.RougeAgent;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+public class CheckMoveTests {
+    private final CheckMove checkMove = new CheckMove();
+    private int rows;
+    private int columns;
+    private AbstractCell[][] cells;
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        rows = 7;
+        columns = 6;
+        cells = new AbstractCell[rows][columns];
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                Feature[] features = new Feature[2];
+                features[0] = Feature.BILLBOARD;
+                features[1] = Feature.BOOKSTORE;
+                cells[row][column] = new NormalCell(features);
+            }
+        }
+        board = new Board(cells);
+    }
+
+    @Test
+    public void testMovementRecruiter() {
+        Recruiter mockedRecruiter = mock(Recruiter.class);
+        when(mockedRecruiter.getId()).thenReturn(1);
+        when(mockedRecruiter.getWalkedPath()).thenReturn(Arrays.asList(new int[] { 4, 3 }));
+
+        board.getCell(5, 3).addPlayer(mockedRecruiter);
+
+        int[][] mask = checkMove.getValidMoves(mockedRecruiter, board, 0);
+
+        int[][] expectedMask = new int[][]
+
+        { { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 0, 1, 1 },
+                { 0, 0, 0, 1, 0, 0 } };
+
+        assertArrayEquals(expectedMask, mask);
+
+        mask = checkMove.getValidMoves(mockedRecruiter, board, 1);
+
+        expectedMask = new int[][]
+
+        { { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 0, 1, 0 },
+                { 0, 0, 0, 1, 0, 0 } };
+
+        assertArrayEquals(expectedMask, mask);
+    }
+
+    @Test
+    public void testMovementAgent() {
+        RougeAgent mockedAgent = mock(RougeAgent.class);
+        when(mockedAgent.getId()).thenReturn(1);
+
+        board.getCell(3, 3).addPlayer(mockedAgent);
+
+        int[][] mask = checkMove.getValidMoves(mockedAgent, board);
+
+        int[][] expectedMask = new int[][]
+
+        { { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, },
+                { 0, 0, 1, 1, 1, 0 },
+                { 0, 1, 1, 1, 1, 1 },
+                { 0, 0, 1, 1, 1, 0 },
+                { 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 } };
+
+        assertArrayEquals(expectedMask, mask);
+    }
+}
