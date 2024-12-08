be7415fe271eb5d78c0e51155c6d978a74be8a5a
Meilin77 <Meilinzhu77@gmail.com>
2024-12-08 21:05:12 +0100

    Add revealedTurns section

--- Project/core/src/main/java/View/screen/GameScreenComponents/TurnBar.java
+++ Project/core/src/main/java/View/screen/GameScreenComponents/TurnBar.java
@@ -1,26 +1,30 @@
 package View.screen.GameScreenComponents;
 
+import java.util.Set;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
-
+import java.util.HashSet;
 import Controller.GameController;
-
 
 public class TurnBar extends Table{
     private final GameController gameController;
     private String timeValue;
     private final Label timeTracker;
+    private final Table pastTurn;
+    private final Set<Table> revealedTurns ;
+    private final Set<Integer> revealedTurnsInt;
+    private final Skin skin;
 
     public TurnBar(GameController gameController){
         this.setDebug(true);
         this.gameController = gameController;
-        Skin skin= new Skin(Gdx.files.internal("comicui/comic-ui.json"));
+        skin= new Skin(Gdx.files.internal("comicui/comic-ui.json"));
 
         Table turnClock = new Table();
-        this.add(turnClock).expandX().fillX();
+        this.add(turnClock).expandX().fillX().top().row();
         this.timeValue=String.valueOf(gameController.getGame().getCurrentTime());
         //not sure if this is necessary in our game,
         //cause it seems the game turn is not gonna more than 9 now.
@@ -29,15 +33,50 @@ public class TurnBar extends Table{
         }
         timeTracker = new Label(timeValue+": 00", skin,"half-tone");
         timeTracker.setAlignment(Align.center);
-        turnClock.add(timeTracker);
+        turnClock.add(timeTracker).expandX().fillX().pad(10);
 
-        Table pastTurn= new Table();
-        this.add(pastTurn).expandX().fillX();
-
+        pastTurn = new Table();
+        this.add(pastTurn).expand().fill().top();
+        revealedTurns =new HashSet<>();
+        revealedTurnsInt=new HashSet<>();
     }
     public void updateTurnbar(){
         String updatedTime=String.valueOf(gameController.getGame().getCurrentTime());
-        timeTracker.setText("0"+updatedTime+": 00");
+        if (updatedTime.length() == 1) {
+            updatedTime = "0" + updatedTime;
+        }
+        timeTracker.setText(updatedTime + ": 00");
+
+        int currentTurn=gameController.getGame().getCurrentTime();
+        if (currentTurn > 5) {
+            if ((currentTurn - 1) % 2 == 1&&!revealedTurnsInt.contains(currentTurn-1)) {
+                revealTurn(currentTurn - 1);
+            }
+        }
+    }
+    private void revealTurn(int turn){
+        String turnString = (turn > 9) ? String.valueOf(turn) : "0" + turn;
+        Table turnRow = new Table();
+        turnRow.setDebug(true);
+        turnRow.align(Align.right);
+
+        Label turnLabel = new Label(turnString + ":00", skin, "half-tone");
+        turnLabel.setAlignment(Align.center);
+        turnRow.add(turnLabel).expandX().fillX().pad(5);
+
+        // Recruits/Mindslip
+
+        // Label actionLabel = new Label("Mindslip", skin, "half-tone");
+        // actionLabel.setAlignment(Align.left);
+        // turnRow.add(actionLabel).expandX().fillX().pad(5);
+
+        if (!revealedTurns.contains(turnRow)) {
+            revealedTurnsInt.add(turn);
+
+            pastTurn.add(turnRow).expandX().fillX().pad(5);
+            pastTurn.row();
+            revealedTurns.add(turnRow);
+        }
     }
 
 }
