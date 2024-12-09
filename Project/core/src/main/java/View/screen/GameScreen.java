a12943cffe5e3cc85f51aee79a940ce719d4b0d4
Vilmavds <78565904+Vilmavds@users.noreply.github.com>
2024-12-09 17:07:25 +0100

    Removed unused imports and variables
    Co-Authored-By: Filip Palmqvist <33493180+mrPaintMan@users.noreply.github.com>
    Co-Authored-By: Meilin77 <145140778+Meilin77@users.noreply.github.com>

--- Project/core/src/main/java/View/screen/GameScreen.java
+++ Project/core/src/main/java/View/screen/GameScreen.java
@@ -4,10 +4,8 @@ import Controller.ActionController;
 import Controller.GameController;
 import View.buildingBlocks.MindMGMTStage;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Array;
@@ -26,7 +24,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import View.screen.GameScreenComponents.TurnBar;
 
 public class GameScreen implements Screen {
-    private final MindMGMT application;
     private final GameController gameController;
     private final ActionController actionController;
     private final MindMGMTStage stage;
@@ -38,7 +35,6 @@ public class GameScreen implements Screen {
     private final SettingWindow settingWindow;
 
     public GameScreen(MindMGMT application) {
-        this.application = application;
 
         this.stage = new MindMGMTStage(new ScreenViewport(), application.assets);
         this.skin = application.skin;
