a3881a54f5cb77052a9c609e7e41c522123fb628
Vilmavds <78565904+Vilmavds@users.noreply.github.com>
2024-12-10 20:03:53 +0100

    Connected button to ActionController

--- Project/core/src/main/java/View/screen/GameScreenComponents/CaptureButton.java
+++ Project/core/src/main/java/View/screen/GameScreenComponents/CaptureButton.java
@@ -8,29 +8,35 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import Controller.GameController;
-import Model.AbstractCell;
-import Model.NormalCell;
+import Model.Board;
+import Model.Player;
+import Model.Recruiter;
 
 public class CaptureButton extends TextButton {
-
-    private AbstractCell cell;
 
     public CaptureButton(GameController gameController, Skin skin) {
 
         super("Capture", skin);
 
-        // TODO: This should probably be in controller
-        this.cell = gameController.getGame().getBoard().getCell(0, 0);
-
         this.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                NormalCell normalCell = (NormalCell) cell;
-                CaptureWindow window = new CaptureWindow(normalCell, true, skin);
+
+                // Fetch all neccecary game data
+                Player player = gameController.getGame().getCurrentPlayer();
+                Recruiter recruiter = gameController.getGame().getRecruiter();
+                Board board = gameController.getGame().getBoard();
+
+                // Perform the capture
+                boolean wasCaptureSuccessful = gameController.actionController.capture(player, recruiter, board);
+
+                // Create a window displaying the result
+                CaptureWindow window = new CaptureWindow(wasCaptureSuccessful, skin);
                 window.setPosition(
                     Gdx.graphics.getWidth() / 2 - window.getWidth() / 2,
                     Gdx.graphics.getHeight() / 2 - window.getHeight() / 2);                
                 actor.getStage().addActor(window);
+
             }
         });
     }
