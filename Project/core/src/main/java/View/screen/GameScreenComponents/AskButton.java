c246351a60de5a55e5440f238b5ae6def653635c
Meilin77 <Meilinzhu77@gmail.com>
2024-12-11 17:18:07 +0100

    Hide askButton for recruiter

--- Project/core/src/main/java/View/screen/GameScreenComponents/AskButton.java
+++ Project/core/src/main/java/View/screen/GameScreenComponents/AskButton.java
@@ -10,10 +10,14 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import Controller.GameController;
 import Model.AbstractCell;
 import Model.NormalCell;
+import Model.Player;
+import Model.RougeAgent;
 
 public class AskButton extends TextButton {
 
     private AbstractCell cell;
+    private final GameController gameController;
+    private Player player;
 
     public AskButton(GameController gameController, Skin skin) {
 
@@ -21,6 +25,7 @@ public class AskButton extends TextButton {
 
         // TODO: This should probbly be in controller
         this.cell = gameController.getGame().getBoard().getCell(0, 0);
+        this.gameController = gameController;
 
         this.addListener(new ChangeListener() {
             @Override
@@ -29,7 +34,7 @@ public class AskButton extends TextButton {
                 AskWindow askActionWindow = new AskWindow(skin, normalCell, gameController);
                 askActionWindow.setPosition(
                     Gdx.graphics.getWidth() / 2 - askActionWindow.getWidth() / 2,
-                    Gdx.graphics.getHeight() / 2 - askActionWindow.getHeight() / 2);                
+                    Gdx.graphics.getHeight() / 2 - askActionWindow.getHeight() / 2);
                 actor.getStage().addActor(askActionWindow);
             }
         });
@@ -47,6 +52,11 @@ public class AskButton extends TextButton {
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
+        this.player = gameController.getGame().getCurrentPlayer();
+        if (player.getClass().equals(RougeAgent.class)){
+            super.draw(batch, parentAlpha); // Important
+        }
+
         // Enable the button
         if (cell.getClass().equals(NormalCell.class)) {
             this.setDisabled(false);
@@ -55,7 +65,6 @@ public class AskButton extends TextButton {
             this.setColor(0.8f, 0.8f, 0.8f, 1f);
             this.setDisabled(true);
         }
-        super.draw(batch, parentAlpha); // Important
     }
 
 }
