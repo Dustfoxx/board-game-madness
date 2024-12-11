5fc076d51f27dd855aa5d6f62a2389f6aef29976
Meilin77 <Meilinzhu77@gmail.com>
2024-12-11 16:06:48 +0100

    Update the Revealbutton
    move the stuff include the button listener
    update when the revealbutton should be shown

--- Project/core/src/main/java/View/screen/GameScreenComponents/RevealButton.java
+++ Project/core/src/main/java/View/screen/GameScreenComponents/RevealButton.java
@@ -18,48 +18,36 @@ import Model.Footstep;
 
 
 public class RevealButton extends TextButton {
+
     private Player player;
+    private final GameController gameController;
+
     public RevealButton(GameController gameController, Skin skin) {
         super("Reveal", skin);
         this.player = gameController.getGame().getCurrentPlayer();
-
-        // I don't think this is the right way to do the reveal action
-        Recruiter recruiter = gameController.getGame().getRecruiter();
-        List<int[]> walkedPath = recruiter.getWalkedPath();
-        Board board = gameController.getGame().getBoard();
-        //int[] playerPosition = board.getPlayerCoord(player);
-        //Footstep footstep =gameController.getGame().getBoard().getCell(playerPosition[0], playerPosition[1]).getFootstep();
-
+        this.gameController = gameController;
 
         this.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                //gameController.actionController.reveal(footstep,board,playerPosition,walkedPath);
+                // Not sure if this is working now
+                Recruiter recruiter = gameController.getGame().getRecruiter();
+                List<int[]> walkedPath = recruiter.getWalkedPath();
+                Board board = gameController.getGame().getBoard();
+                int[] playerPosition = board.getPlayerCoord(player);
+                Footstep footstep =gameController.getGame().getBoard().getCell(playerPosition[0], playerPosition[1]).getFootstep();
+                gameController.actionController.reveal(footstep,board,playerPosition,walkedPath);
             }
         });
     }
 
-
-    //Probably not working now
-    @Override
-    public void act(float delta) {
-        super.act(delta);
-
-        // Only visible and enabled for RougeAgent
-        if (player.getClass().equals(RougeAgent.class)) {
-            this.setVisible(true);
-            this.setDisabled(false);
-        } else {
-            this.setVisible(false);
-            this.setDisabled(true);
-        }
-    }
-
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        if (this.isVisible()) {
-            super.draw(batch, parentAlpha);
-        }
+            this.player = gameController.getGame().getCurrentPlayer();
+            if (player.getClass().equals(RougeAgent.class)){
+                super.draw(batch, parentAlpha);
+            }
     }
+    
 }
 
