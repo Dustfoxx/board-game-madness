c431c00e0144378c90f29c5ca9f4c7661535c6cc
Vilmavds <78565904+Vilmavds@users.noreply.github.com>
2024-12-11 17:08:35 +0100

    Added visuals to screen

--- Project/core/src/main/java/View/screen/PlayersScreen.java
+++ Project/core/src/main/java/View/screen/PlayersScreen.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -18,44 +20,56 @@ public class PlayersScreen implements Screen {
     private final MindMGMTStage stage;
     private final MindMGMT application;
 
+    private String[] names;
+
     public PlayersScreen(final MindMGMT application) {
         this.application = application;
+        this.names = new String[application.nrOfPlayers];
         stage = new MindMGMTStage(new ScreenViewport(), application.assets);
         setupUI();
     }
 
-    private void startGame() {
-        application.setScreen(new GameScreen(application));
-        dispose();
-    }
-
     private void setupUI() {
         Table root = new Table();
+        root.setDebug(true);
         root.setFillParent(true);
 
-        float width = stage.getWidth();
-        float height = stage.getHeight();
-
-        TextButton playersButton = new TextButton("Start game", application.skin);
-        root.add(playersButton).pad(20);
-        playersButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                startGame();
+        for (int i = 0; i < names.length; i++ ) {
+            Label nameLabel;
+            if (i == 0) {
+                nameLabel = new Label("Recruiter", application.skin);
+            } else {
+                nameLabel = new Label("Agent " + i, application.skin);
             }
-        });
+            nameLabel.setFontScale(2);
+            TextField nameField = new TextField(names[i], application.skin);
+            root.add(nameLabel).colspan(2).width(500).left();
+            root.row();
+            root.add(nameField).colspan(2).width(500).fillX();
+            root.row();
+        }
 
-        root.row();
-
-        TextButton backButton = new TextButton("Back", application.skin);
-        backButton.addListener(new ChangeListener() {
+        TextButton startButton = new TextButton("Start game", application.skin);
+        startButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                application.setScreen(new MainMenuScreen(application));
+                application.setScreen(new GameScreen(application));
                 dispose();
             }
         });
-        root.add(backButton).colspan(2).width(width * 0.1f).height(height * 0.05f);
+
+        TextButton backButton = new TextButton("Back", application.skin);
+        backButton.setSize(startButton.getWidth(), startButton.getHeight());
+        backButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                application.setScreen(new SetupScreen(application));
+                dispose();
+            }
+        });
+
+        root.add(backButton);
+        root.add(startButton);
         stage.addActor(root);
         Gdx.input.setInputProcessor(stage);
     }
